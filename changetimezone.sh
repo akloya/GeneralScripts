f692d5b109defcbe55440ecbfc12aeb5d45c8e4f
akloya <ajaykumar.loya@gmail.com>
2017-05-24 11:40:43 -0700

    Update changetimezone.sh

--- changetimezone.sh
+++ changetimezone.sh
@@ -2,4 +2,5 @@
 # Backup Existing localtime 
 sudo mv /etc/localtime /etc/localtime.bak
 # Setting up Local Time to America/Los_Angeles
+# if you are unsure abt what timezone path is your look into the folder /usr/share/zoneinfo ... and use the exact path
 sudo ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
